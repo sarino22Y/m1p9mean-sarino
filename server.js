c29e5b918bb0b5c81e537abcd5a6c426354a05ec
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-01 11:54:38 +0300

    Debug server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-app-heroku'));
+app.use(express.static(__dirname + '/dist/m1p9mean-sarino'));
 
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
     // res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
     // res.sendFile('index.html', {root: 'dist/'}),
-    res.sendFile(path.join(__dirname + '/dist/angular-app-heroku/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/m1p9mean-sarino/index.html'))
 );
 
 // Start the app by listening on the default Heroku port
