54a674ce7d0fad2cf00fbaad0b314ce9f808db15
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-10 11:16:31 +0300

    Modals pour les liens de connexion et inscription.

--- src/app/components/shared/headers/headerloggedin/headerloggedin.component.ts
+++ src/app/components/shared/headers/headerloggedin/headerloggedin.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 
+declare var window: any;
+
 @Component({
   selector: 'app-headerloggedin',
   templateUrl: './headerloggedin.component.html',
@@ -10,15 +12,53 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class HeaderloggedinComponent implements OnInit {
 
-  name:any;
+  name: any;
+  loginModal: any;
+  registerModal: any;
+
   constructor(
     private userService: UserService ,
     private route: Router
   ) { }
 
   ngOnInit(): void {
-    
+    this.loginModal = new window.bootstrap.Modal(
+      document.getElementById("linksloginmodal")
+    );
+    this.registerModal = new window.bootstrap.Modal(
+      document.getElementById("linksregistermodal")
+    );
   }
+
+  // Modals Login :
+
+  openLoginModal(){
+    this.loginModal.show();
+  }
+
+  hideLoginModal(){
+    this.loginModal.hide();
+  }
+
+  closeLoginModal(){
+    this.loginModal.hide();
+    this.route.navigate(['']);
+  }
+
+    // Modals Register :
+
+    openRegisterModal(){
+      this.registerModal.show();
+    }
+  
+    hideRegisterModal(){
+      this.registerModal.hide();
+    }
+  
+    closeRegisterModal(){
+      this.registerModal.hide();
+      this.route.navigate(['']);
+    }
 
   nameUser():any{
     return this.userService.nameOfUserConnected();
