a299d258d0bbc3f93b66cc794b50dd81dad14871
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-10 18:52:11 +0300

    Route pour le tableau de bord.

---
+++ src/app/routes/dashboard-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+
+import { ClientdashboardComponent } from '../components/dashboard/clientdashboard/clientdashboard.component';
+import { RestaurantdashboardComponent } from '../components/dashboard/restaurantdashboard/restaurantdashboard.component';
+import { DelivererdashboardComponent } from '../components/dashboard/delivererdashboard/delivererdashboard.component';
+
+const routes: Routes = [
+    {path: 'client/dashboard', component: ClientdashboardComponent},
+    {path: 'restaurant/dashboard', component: RestaurantdashboardComponent},
+    {path: 'deliverer/dashboard', component: DelivererdashboardComponent}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class DashboardRoutingModule { }
