cb710f125262240e7fc52fbdf406b1eeef89ed57
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-11 14:00:49 +0300

    Insert un commande par le responsable de Ekaly. (TODO : Normalement, Ekaly doit voir seulement les commandes en cours.)

--- src/app/components/commande/commande.component.ts
+++ src/app/components/commande/commande.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Icommande } from 'src/app/models/icommande';
 import { CommandeService } from 'src/app/services/commande.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-commande',
@@ -7,12 +11,67 @@ import { CommandeService } from 'src/app/services/commande.service';
   styleUrls: ['./commande.component.css']
 })
 export class CommandeComponent implements OnInit {
+
+  commandeForm!: FormGroup;
+  model!: Icommande;
   commandes: any;
 
-  constructor(private commandeService:CommandeService) { }
+  nameClientC: any;
+  adressClientC: any;
+  mailClientC: any;
+
+  constructor(
+    private commandeService: CommandeService,
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private route: Router
+    ) { 
+      this.infoUser();
+    }
 
   async ngOnInit() {
     await this.getListeCommande();
+    this.infoUser();
+    this.userService.updateTheMenu.subscribe(res => {
+      this.createForm();       
+    });    
+    this.createForm();
+  }
+
+  /**
+   * Form de la commande.
+   */
+  createForm() {
+    this.commandeForm = this.formBuilder.group({
+      namePlat: new FormControl('', [Validators.required]),
+      nombre: new FormControl('', [Validators.required]),
+      dateLivraison: new FormControl('', [Validators.required])
+    });
+  }
+
+  get f(){
+    return this.commandeForm.controls;
+  }
+
+  // Nom d'utilisateur connecté.
+  nameUser():any{
+    return this.userService.nameOfUserConnected();
+  }
+
+  // Id d'utilisateur connecté.
+  idUser():any{
+    return this.userService.idOfUserConnected();
+  }
+
+  infoUser() {
+    console.log('IDDusereser',this.idUser());
+    
+    return this.userService.getUserById(this.idUser()).subscribe( res => {
+      this.nameClientC =  res[0].name;
+      this.adressClientC =  res[0].adress;
+      this.mailClientC =  res[0].email;
+      console.log('ITYYRESESESE', res[0].email);
+    })
   }
 
   /**
@@ -25,4 +84,25 @@ export class CommandeComponent implements OnInit {
       console.log('ITYYYYYYYYYYYYYYYY',this.commandes);
     });
   }
+
+  /**
+   * Ajout de commande.
+   */
+   addCommande() {
+    this.model = this.commandeForm.value;
+
+    this.model["nameClient"] = this.nameClientC;
+    this.model["adressClient"] = this.adressClientC;
+    this.model["emailClient"] = this.mailClientC;
+    // console.log(this.model);
+    this.commandeService.create(this.model)
+    .subscribe({
+      next: ( data ) => {
+        console.log("DATA--------------------",data);
+      },
+      error: (e) => {
+        console.error(e);
+      }
+    })
+   }
 }
---
+++ src/app/models/icommande.ts
@@ -0,0 +1,8 @@
+export interface Icommande {
+    namePlat: string,
+    nameClient: string,
+    adressClient: string,
+    emailClient: string,
+    nombre: number,
+    dateLivraison: Date
+}
