40c8482eb76c88e313375901a1d40ab0f8d04249
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-11 14:01:15 +0300

    Inserer des commandes par le client connecté.

--- src/app/components/shared/accueil/accueil.component.ts
+++ src/app/components/shared/accueil/accueil.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Icommande } from 'src/app/models/icommande';
+import { CommandeService } from 'src/app/services/commande.service';
 import { PlatService } from 'src/app/services/plat-service.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -20,11 +23,21 @@ export class AccueilComponent implements OnInit {
   displayCommande: any;
   curentRole: any;
 
+  commandeForm!: FormGroup;
+  model!: Icommande;
+  nameClientC: any;
+  adressClientC: any;
+  mailClientC: any;
+
   constructor(
     private platListeService: PlatService,
     private userService: UserService,
+    private commandeService: CommandeService,
+    private formBuilder: FormBuilder,
     private route: Router
-    ) { }
+    ) { 
+      this.infoUser();
+    }
 
   async ngOnInit() {
 
@@ -37,9 +50,15 @@ export class AccueilComponent implements OnInit {
     await this.getListePlat();
 
     this.userService.updateTheMenu.subscribe(res => {
-      this.displayButtonCommandForClientAndVisitor();       
+      this.displayButtonCommandForClient();       
     });    
-    this.displayButtonCommandForClientAndVisitor();
+    this.displayButtonCommandForClient();
+
+    this.infoUser();
+    this.userService.updateTheMenu.subscribe(res => {
+      this.createForm();       
+    });    
+    this.createForm();
   }
 
   // Modals Commande :
@@ -53,20 +72,73 @@ export class AccueilComponent implements OnInit {
     this.route.navigate(['']);
   }
 
-  async commander(){
-    return "this.";
+  /**
+   * Form de la commande.
+   */
+    createForm() {
+      this.commandeForm = this.formBuilder.group({
+        namePlat: new FormControl('', [Validators.required]),
+        nombre: new FormControl('', [Validators.required]),
+        dateLivraison: new FormControl('', [Validators.required])
+      });
+    }
+
+   // Id d'utilisateur connecté.
+  idUser():any{
+    return this.userService.idOfUserConnected();
   }
+
+  /**
+   * Informations de l'utilisateur connecté.
+   * @returns 
+   */
+  infoUser() {
+    console.log('IDDusereser',this.idUser());
+    
+    return this.userService.getUserById(this.idUser()).subscribe( res => {
+      this.nameClientC =  res[0].name;
+      this.adressClientC =  res[0].adress;
+      this.mailClientC =  res[0].email;
+      console.log('ITYYRESESESE', res[0].email);
+    })
+  }
+
+   /**
+   * Ajout de commande.
+   */
+    commander() {
+      this.model = this.commandeForm.value;
+  
+      this.model["nameClient"] = this.nameClientC;
+      this.model["adressClient"] = this.adressClientC;
+      this.model["emailClient"] = this.mailClientC;
+      // console.log(this.model);
+      this.commandeService.create(this.model)
+      .subscribe({
+        next: ( data ) => {
+          console.log("DATA--------------------",data);
+        },
+        error: (e) => {
+          console.error(e);
+        }
+      })
+     }
 
   /**
    * Afficher le bouton commande sur l'accueil si 
    * l'utilisateur connecté est un client.
    */
-  displayButtonCommandForClientAndVisitor(){
+  displayButtonCommandForClient(){
     if (this.userService.getToken() != '') {
       this.curentRole = this.userService.getRoleByToken(this.userService.getToken());
       // Client seulement
       this.displayCommande = this.curentRole == 'client';
     }
+  }
+
+   // Si un utilisateur est connecté.
+  loggedIn(){
+    return localStorage.getItem('token');
   }
 
   /**
