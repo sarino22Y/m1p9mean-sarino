3281ab8f070bc584778a73adde61f5e6034efcbe
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-16 12:08:04 +0300

    Debug redirect in HEROKU.

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/m1p9mean-sarino'));
+app.use(express.static(__dirname + '/dist/m1p9mean-sarino'));
+
+app.get('', (req, res) =>
+    // res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    // res.sendFile('index.html', {root: 'dist/'}),
+    res.sendFile(path.join(__dirname + '/dist/m1p9mean-sarino/index.html'))
+);
 
 app.get('/', (req, res) =>
     // res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
     // res.sendFile('index.html', {root: 'dist/'}),
-    res.sendFile(path.join(__dirname + '/m1p9mean-sarino'))
+    res.sendFile(path.join(__dirname + '/dist/m1p9mean-sarino/index.html'))
+);
+
+app.get('/*', (req, res) =>
+    // res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    // res.sendFile('index.html', {root: 'dist/'}),
+    res.sendFile(path.join(__dirname + '/dist/m1p9mean-sarino/index.html'))
 );
 
 // Start the app by listening on the default Heroku port
