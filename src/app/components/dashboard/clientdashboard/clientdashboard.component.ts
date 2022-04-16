bc065e8a9e3fc2cd9d6dca728940be778ddd8350
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-16 11:00:59 +0300

    Debut de création de tableau de bord client.

--- src/app/components/dashboard/clientdashboard/clientdashboard.component.ts
+++ src/app/components/dashboard/clientdashboard/clientdashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Icommande } from 'src/app/models/icommande';
 import { CommandeService } from 'src/app/services/commande.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,11 +14,19 @@ export class ClientdashboardComponent implements OnInit {
   title!: string;
   commandes!: any;
   commandesCurrentUser: any;
+  nameClientC!: any;
+  adressClientC: any;
+  mailClientC: any;
+
   nameCurrentClient: any;
+  
+  commandeForm!: FormGroup;
+  model!: Icommande;
 
   constructor(
     private commandeService: CommandeService,
-    private userService: UserService
+    private userService: UserService,
+    private formBuilder: FormBuilder
     ) {  
       
     }
@@ -25,24 +35,59 @@ export class ClientdashboardComponent implements OnInit {
     this.title = 'Tableau de board';
     await this.getListeCommande();        
     this.infoUser();
+    this.createForm();
   }
+
+  /**
+   * Form de la commande.
+   * 
+  */
+  createForm() {
+    this.commandeForm = this.formBuilder.group({
+      namePlat: new FormControl('', [Validators.required]),
+      nombre: new FormControl('', [Validators.required]),
+      dateLivraison: new FormControl('', [Validators.required])
+    });
+  }
+
+  /**
+ * Informations de l'utilisateur connecté.
+ * @returns 
+ */
+  infoUser() {
+    console.log('Id User',this.idUser());
+    
+    return this.userService.getUserById(this.idUser()).subscribe( res => {
+      this.nameClientC =  res[0].name;
+      this.adressClientC =  res[0].adress;
+      this.mailClientC =  res[0].email;
+    })
+  }
+  
+  /**
+   * Ajout de commande.
+  */
+  commander() {
+    this.model = this.commandeForm.value;
+
+    this.model["nameClient"] = this.nameClientC;
+    this.model["adressClient"] = this.adressClientC;
+    this.model["emailClient"] = this.mailClientC;
+    // console.log(this.model);
+    this.commandeService.create(this.model)
+    .subscribe({
+      next: ( data ) => {
+        console.log("DATA--------------------",data);
+      },
+      error: (e) => {
+        console.error(e);
+      }
+    })
+   }
 
   // Id d'utilisateur connecté.
   idUser():any{
     return this.userService.idOfUserConnected();
-  }
-
-  /**
-   * Information sur l'utilisateur conecté.
-   * @returns 
-   */
-  infoUser() {
-    console.log('IDDusereser',this.idUser());
-    
-    return this.userService.getUserById(this.idUser()).subscribe( res => {
-      this.nameCurrentClient =  res[0].name;
-      console.log('RESSS', this.nameCurrentClient);
-    })
   }
 
   /**
