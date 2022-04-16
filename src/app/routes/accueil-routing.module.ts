57fa4f67975a11c9b1e372ddb155eeb13e51e85b
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-16 11:21:10 +0300

    Correction redirection.

--- src/app/routes/accueil-routing.module.ts
+++ src/app/routes/accueil-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule} from '@angular/router';
 import { AccueilComponent } from '../components/shared/accueil/accueil.component';
 
 const routes: Routes = [
-    {path: '', component: AccueilComponent}
+    {path: '', component: AccueilComponent},
+    {path: 'm1p9mean-sarino', component: AccueilComponent}
 ]
 
 @NgModule({
