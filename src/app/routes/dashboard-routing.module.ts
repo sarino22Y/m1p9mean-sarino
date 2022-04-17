d8db23a0ebdc791cfc8b38fdf6e956b835fe28b7
sarino22Y <sarino.malalanirina@esti.mg>
2022-04-18 00:02:03 +0300

    Consulter seulement par profil ekaly.

--- src/app/routes/dashboard-routing.module.ts
+++ src/app/routes/dashboard-routing.module.ts
@@ -5,12 +5,13 @@ import { ClientdashboardComponent } from '../components/dashboard/clientdashboar
 import { RestaurantdashboardComponent } from '../components/dashboard/restaurantdashboard/restaurantdashboard.component';
 import { DelivererdashboardComponent } from '../components/dashboard/delivererdashboard/delivererdashboard.component';
 import { EkalydashboardComponent } from '../components/dashboard/ekalydashboard/ekalydashboard.component';
+import { RoleGuard } from '../shared/auth/role.guard';
 
 const routes: Routes = [
     {path: 'client/dashboard', component: ClientdashboardComponent},
     {path: 'restaurant/dashboard', component: RestaurantdashboardComponent},
     {path: 'deliverer/dashboard', component: DelivererdashboardComponent},
-    {path: 'ekaly/dashboard', component: EkalydashboardComponent}
+    {path: 'ekaly/dashboard', component: EkalydashboardComponent, canActivate:[RoleGuard]}
 ]
 
 @NgModule({
