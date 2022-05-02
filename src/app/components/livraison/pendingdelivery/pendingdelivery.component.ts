07465ea9f58d2a91e0f20662f43eacea074aadf1
sarino22Y <sarino.malalanirina@esti.mg>
2022-05-02 22:34:46 +0300

    Synchronisation de suppression:     Livraison,     LivraisonInfo

--- src/app/components/livraison/pendingdelivery/pendingdelivery.component.ts
+++ src/app/components/livraison/pendingdelivery/pendingdelivery.component.ts
@@ -231,22 +231,24 @@ export class PendingdeliveryComponent implements OnInit {
   /**
    * Supprimmer une livaraison
    */
-  async delete(idDelivery: any) {
+  async delete(idDeliveryInfo: any, idDelivery: any) {
     let name: string;
     this.livraisonService.getOne(idDelivery).subscribe( res => {
 
-    if (confirm("Supprimmer la livraison")) {       
-      this.livraisonService.delete(idDelivery).subscribe({
-        next: ( data ) => {
-          window.location.href= "/livraisons";
-          console.log("DATA-----",data);
-        },
-        error: (e) => {
-          alert("Une erreur s'est produite. Veuillez réessayer plus tard.")
-          console.error(e);
-        }
-      })
-    }
+      if (confirm("Supprimmer la livraison")) {     
+        // alert("Pas disponible pour l'instant, réessayer plus tars.")  
+        this.livraisonService.deleteInfo(idDeliveryInfo).subscribe({
+          next: ( data ) => {
+            window.location.href= "/livraisons/pending";
+            console.log("DATA-----",data);
+            this.livraisonService.delete(idDelivery).subscribe()
+          },
+          error: (e) => {
+            alert("Une erreur s'est produite. Veuillez réessayer plus tard.")
+            console.error(e);
+          }
+        })
+      }
   }); 
   }
 
