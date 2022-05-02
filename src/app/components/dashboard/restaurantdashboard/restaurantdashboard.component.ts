ee1b203c17917b7b49a822b43ebfc844652eb54a
sarino22Y <sarino.malalanirina@esti.mg>
2022-05-02 15:16:01 +0300

    Correction de tableau de bord de restaurant.

--- src/app/components/dashboard/restaurantdashboard/restaurantdashboard.component.ts
+++ src/app/components/dashboard/restaurantdashboard/restaurantdashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LivraisonService } from 'src/app/services/livraison.service';
 import { PlatService } from 'src/app/services/plat.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,10 +13,12 @@ export class RestaurantdashboardComponent implements OnInit {
   title!: string;
   Plats!: any;
   nbrplats: any;
+  nbrplatsSold: any;
   profit: any;
 
   constructor(
     private platService: PlatService,
+    private livraisonService: LivraisonService,
     private userService: UserService
     ) { }
 
@@ -30,7 +33,7 @@ export class RestaurantdashboardComponent implements OnInit {
    */
      getListePlats(){
       let id = this.userService.idOfUserConnected();
-      return this.platService.getAll().subscribe( res => {
+      this.platService.getAll().subscribe( res => {
         let arrayPlatRestaurantConnected = [];
         let profitTotal = [];
         for (let i = 0; i < res["plats"].length; i++) {
@@ -42,6 +45,29 @@ export class RestaurantdashboardComponent implements OnInit {
         this.profit =  profitTotal.reduce((partialSum, a) => parseInt(partialSum) + parseInt(a), 0);
         this.nbrplats = arrayPlatRestaurantConnected.length;
       })
+
+      let platsSold: any = [];
+      let resultSoldPerRestaurant: any = [];
+      this.livraisonService.getAll().subscribe(resLivraison => {
+        this.platService.getAll().subscribe(resplats => {
+          for (let i = 0; i < resplats.plats.length; i++) {
+            for (let j = 0; j < resLivraison.livraisons.length; j++) {
+              if (resLivraison.livraisons[j].idPlat == resplats.plats[i]._id) {
+                platsSold[j] = resplats.plats[i];
+              }     
+            }
+          }
+          for (let k = 0; k < platsSold.length; k++) {           
+            if (platsSold[k].idRestaurant == id) {
+              resultSoldPerRestaurant.push(platsSold[k])
+            }
+          }
+          console.log("Plat resultSoldPerRestaurant ty", resultSoldPerRestaurant);
+          console.log("platsSold ty", platsSold);
+          
+          this.nbrplatsSold = resultSoldPerRestaurant.length;
+        })
+      });
     }
 
 }
