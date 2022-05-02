234a4797a50b3fbacf6f82d0cdc80c723dfaab39
sarino22Y <sarino.malalanirina@esti.mg>
2022-05-02 14:55:49 +0300

    Route pour la reste et les plats vendus.

--- src/app/routes/plat-routing.module.ts
+++ src/app/routes/plat-routing.module.ts
@@ -3,9 +3,13 @@ import { Routes, RouterModule} from '@angular/router';
 import { PlatComponent } from '../components/plat/plat.component';
 import { AuthGuard } from '../shared/auth/auth.guard';
 import { RoleGuard } from '../shared/acces/role.guard';
+import { PlatSoldComponent } from '../components/plat/plat-sold/plat-sold.component';
+import { PlatRemainsComponent } from '../components/plat/plat-remains/plat-remains.component';
 
 const routes: Routes = [
     {path: 'platliste', component: PlatComponent},
+    {path: 'platliste/sold', component: PlatSoldComponent},
+    {path: 'platliste/remains', component: PlatRemainsComponent},
     {path: 'platliste/:platId', component: PlatComponent},
 
 ]
